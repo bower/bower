e1a3b21bb46a88e559c068568231562b9dd851e5
Rod Vagg <rod@vagg.org>
2012-09-09 21:00:25 +1000

    rebuild

--- build/index.js
+++ build/index.js
@@ -5,10 +5,11 @@ var markdown = require('markdown').markdown;
 var fs       = require('fs');
 // enable GitHub Flavored Markdown dialect
 require('./gfm-dialect.js')
-var url      = "https://raw.github.com/twitter/bower/master/README.md?login=fat&token=90f86d018a7090d9161c74f9096764be";
+var url      = "https://raw.github.com/twitter/bower/master/README.md";
 var template = Hogan.compile(fs.readFileSync(path.join(__dirname,'./template.{'), 'utf-8'));
 
 request(url, function (error, response, body) {
+  body = body.replace(/ \[\!\[Build Status\].+$/m, '')
   var bodyHTML = markdown.toHTML(body, 'GitHub');
   fs.writeFileSync(path.join(__dirname, '../index.html'), template.render({ body: bodyHTML }));
 });
