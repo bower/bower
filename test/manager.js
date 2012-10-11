fd31f247a623cf466b684a621814d05d67858770
Andre Cruz <andremiguelcruz@msn.com>
2012-10-11 13:40:54 +0100

    Unit of work implementation.
    A unit of work is a simple storage with write lock/unlock.
    The manager/package now share a unit of work instance.
    The unit of work is used to prevent shared dependencies from being cloned/copied "at the same time" fixing issue #81.
    The prune and version resolving algorithm was also not correct. It now resolves versions correctly, fixing issue #57.
    
    - Fix I/O errors caused by copying/clone repos simultaneously to the same dest.
    - Optimize the clone/copy step by avoiding it if the last resolved resource is the same.
    - Fix failing test (at least on my windows machine)
    - Add some more tests
    - Fix CS.

---
+++ test/manager.js
@@ -0,0 +1,68 @@
+var assert  = require('assert');
+var path    = require('path');
+// var fs      = require('fs');
+// var _       = require('lodash');
+var Manager = require('../lib/core/manager');
+var rimraf  = require('rimraf');
+var config  = require('../lib/core/config');
+
+describe('manager', function () {
+  beforeEach(function (done) {
+    rimraf(config.directory, function (err) {
+      if (err) {
+        throw new Error('Unable to delete local directory.');
+      }
+      done();
+    });
+  });
+
+  it('Should resolve JSON dependencies', function (next) {
+    var manager = new Manager([]);
+    manager.cwd = __dirname + '/assets/project';
+
+    manager.on('resolve', function () {
+      assert.deepEqual(manager.dependencies["jquery-ui"][0].version, "1.8.23");
+      assert.deepEqual(manager.dependencies["jquery"][0].version, "1.8.1");
+      assert.deepEqual(manager.dependencies["package-bootstrap"][0].version, "2.0.6");
+
+      rimraf(config.directory, function (err) {
+        next();
+      });
+    });
+
+    manager.resolve();
+
+  });
+
+  it('Should resolve nested JSON dependencies', function (next) {
+    var manager = new Manager([]);
+    manager.cwd = __dirname + '/assets/other-project';
+
+    manager.on('resolve', function () {
+      assert.deepEqual(manager.dependencies["jquery"][0].version, "1.7.2");
+      assert.deepEqual(manager.dependencies["jquery-pjax"][0].version, "1.0.0");
+
+      rimraf(config.directory, function (err) {
+        next();
+      });
+    });
+
+    manager.resolve();
+  });
+
+  it('Should detect unresolvable packages in nested JSON dependencies', function (next) {
+    var manager = new Manager([]);
+    manager.cwd = __dirname + '/assets/conflict-project';
+
+    var detected = false;
+    manager.on('error', function (err) {
+      if (/no resolvable.* jquery$/i) detected = true;
+    });
+    manager.on('resolve', function () {
+      if (!detected) throw new Error('A conflict in jquery should have been detected.');
+      next();
+    });
+
+    manager.resolve();
+  });
+});
