d561299cdf052c51f30ff106f2bba6f4428e22d4
André Cruz <andremiguelcruz@msn.com>
2012-11-29 21:46:01 +0000

    Fix perm issue with downloaded files, fixes #160.

--- test/package.js
+++ test/package.js
@@ -294,7 +294,7 @@ describe('package', function () {
     pkg.resolve();
   });
 
-  it('Should have accessible file permissions on temp folder', function (next) {
+  it('Should have accessible file permissions', function (next) {
     var pkg = new Package('jquery', 'git://github.com/maccman/package-jquery.git');
     var cachePath;
 
@@ -314,6 +314,29 @@ describe('package', function () {
       async.map([pkg.localPath, cachePath], fs.stat, function (err, results) {
         if (err) throw new Error(err);
         assert.equal(results[0].mode, results[1].mode);
+        assert.equal(results[0].mode, 16822);  // means 0777
+        next();
+      });
+    });
+
+    pkg.resolve();
+  });
+
+  it('Should have accessible file permissions for downloaded files', function (next) {
+    var pkg = new Package('bootstrap', 'http://twitter.github.com/bootstrap/assets/bootstrap.zip');
+
+    pkg.on('resolve', function () {
+      pkg.install();
+    });
+
+    pkg.on('error', function (err) {
+      throw new Error(err);
+    });
+
+    pkg.on('install', function () {
+      fs.stat(pkg.localPath, function (err, stat) {
+        if (err) throw new Error(err);
+        assert.equal(stat.mode, 16822);  // means 0777
         next();
       });
     });
