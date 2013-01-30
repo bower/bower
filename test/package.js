5d45b04b224a00589cff2ff8288978074c82b3e0
David DeSandro <desandrocodes@gmail.com>
2013-01-30 11:29:06 -0500

    fix binding install; add test for removing ignorables

--- test/package.js
+++ test/package.js
@@ -2,9 +2,11 @@
 
 var assert  = require('assert');
 var fs      = require('fs');
+var path    = require('path');
 var nock    = require('nock');
 var _       = require('lodash');
 var rimraf  = require('rimraf');
+var glob    = require('glob');
 var async   = require('async');
 var config  = require('../lib/core/config');
 var Package = require('../lib/core/package');
@@ -423,6 +425,38 @@ describe('package', function () {
         assert(files.indexOf('img') !== -1);
         next();
       });
+    });
+
+    pkg.resolve();
+  });
+
+  it('Should remove ignored filepaths', function (next) {
+    var pkg = new Package('turtles', __dirname + '/assets/package-ignorables');
+
+    pkg.on('resolve', function () {
+      pkg.install();
+    });
+
+    pkg.on('error', function (err) {
+      throw new Error(err);
+    });
+
+    var pkgInstallPath = path.join(__dirname, '/../components/turtles/');
+    pkg.on('install', function () {
+      // these files should have been deleted
+      assert(!fs.existsSync(pkgInstallPath + 'don.txt'));
+      assert(!fs.existsSync(pkgInstallPath + 'leo.txt'));
+      assert(!fs.existsSync(pkgInstallPath + '/test/'));
+      // this file should still be there
+      assert(fs.existsSync(pkgInstallPath + 'index.js'));
+      // all ignore file pattern should be removed
+      async.forEach(pkg.json.ignore, function (ignorePattern, asyncNext) {
+        var pattern = path.join(__dirname, '/../components/turtles/' + ignorePattern);
+        glob(pattern, function (err, globPath) {
+          assert(globPath.length === 0);
+          asyncNext();
+        });
+      }, next);
     });
 
     pkg.resolve();
