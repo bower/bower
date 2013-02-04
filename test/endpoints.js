47a55c2bc4c351e0c762231815aeb9f152bc015c
Nick Heiner <nick.heiner@opower.com>
2013-02-04 15:56:50 -0500

    Adding configurable endpoints for search(), lookup(), info(), and all(), as well as tests for search()

---
+++ test/endpoints.js
@@ -0,0 +1,68 @@
+var assert = require('assert');
+var source = require('../lib/core/source');
+var nock   = require('nock');
+var _      = require('lodash');
+
+describe('endpoints', function () {
+
+  describe('search', function() {
+
+    it('Should use the default endpoints if none are specified', function (next) {
+
+      var expected = [
+        {
+          name: 'jquery',
+          url: 'path/to/jquery',
+          endpoint: undefined
+        },
+        {
+          name: 'jquery-ui',
+          url: 'path/to/jquery-ui',
+          endpoint: undefined
+        }
+      ];
+
+      nock('https://bower.herokuapp.com')
+          .get('/packages/search/jquery')
+          .reply(200, expected);
+
+      source.search('jquery', function(err, results) {
+        assert.deepEqual(results, expected);
+        next();
+      });
+    });
+
+    it('Should use the specified endpoints', function (next) {
+
+      var expected1 = [{
+          name: 'jquery',
+          url: 'path/to/jquery',
+          endpoint: undefined
+      }];
+
+      var expected2 = [{
+          name: 'jquery-ui',
+          url: 'path/to/jquery-ui',
+          endpoint: undefined
+      }]
+
+      nock('https://endpoint1.com')
+          .get('/packages/search/jquery')
+          .reply(200, expected1);
+
+      nock('https://endpoint2.com')
+          .get('/packages/search/jquery')
+          .reply(200, expected2);
+
+      source.search('jquery', function(err, results) {
+        assert(_.find(results, function(item) {return _.isEqual(item, expected1[0])}));
+        assert(_.find(results, function(item) {return _.isEqual(item, expected2[0])}));
+        assert(results.length === 2);
+
+        next();
+      }, ['https://endpoint1.com/packages', 'https://endpoint2.com/packages']);
+
+    });
+  });
+
+});
