e0454243248de1c6fbf907862d3b7ca8390c964d
Nick Heiner <nick.heiner@opower.com>
2013-02-06 17:22:05 -0500

    Changing search to emit a 'results' event instead of 'result'. Also removing register tests

--- test/command.js
+++ test/command.js
@@ -11,8 +11,8 @@ describe('command', function() {
           .get('/packages/search/asdf')
           .reply(200, {});
 
-      commands.search('asdf', {}).on('result', function(result) {
-        assert.deepEqual([], result);
+      commands.search('asdf', {}).on('results', function(results) {
+        assert.deepEqual([], results);
         next();
       });
     });
@@ -29,8 +29,8 @@ describe('command', function() {
           .get('/packages/search/fawagahds')
           .reply(200, expected);
 
-      commands.search('fawagahds', {}).on('result', function(result) {
-        assert.deepEqual(result, expected);
+      commands.search('fawagahds', {}).on('results', function(results) {
+        assert.deepEqual(results, expected);
         next();
       });
     });
@@ -43,7 +43,7 @@ describe('command', function() {
 
   describe('lookup', function() {
 
-    it('Should emit a results event for lookup when nothing is found', function(next) {
+    it('Should emit a result event for lookup when nothing is found', function(next) {
       nock('https://bower.herokuapp.com')
           .get('/packages/asdf')
           .reply(404);
@@ -54,7 +54,7 @@ describe('command', function() {
       });
     });
 
-    it('Should emit a results event for lookup when something is found', function(next) {
+    it('Should emit a result event for lookup when something is found', function(next) {
       var expected = {
         name: 'fawagahds-mobile',
         url: 'git://github.com/strongbad/fawagahds-mobile.js'
@@ -72,28 +72,6 @@ describe('command', function() {
       afterEach(function() {
         nock.cleanAll();
       });
-    });
-
-  });
-
-  describe('register', function() {
-
-    it('Should return true if registration succeeded', function(next) {
-      var name = 'oh-yeah',
-          url = 'repo-oh-yeah.js';
-
-      nock('https://bower.herokuapp.com')
-          .post('/packages', 'name=' + name + '&url=' + url)
-          .reply(201);
-
-      commands.register(name, url, {silent: true}).on('result', function(result) {
-        assert(result);
-        next();
-      });
-    });
-
-    afterEach(function() {
-      nock.cleanAll();
     });
 
   });
