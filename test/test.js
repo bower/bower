a4fbe65259d7aec402020ff9072ec8937f6f68ae
André Cruz <andremiguelcruz@msn.com>
2013-04-14 15:27:08 +0100

    Implemented GitFsResolver, minor refactor to the GitRemoteResolver.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
-var GitResolver = require('../lib/resolve/resolvers/GitRemoteResolver');
+var GitRemoteResolver = require('../lib/resolve/resolvers/GitRemoteResolver');
+var GitFsResolver = require('../lib/resolve/resolvers/GitFsResolver');
 
-function testGitResolver() {
-    var dejavuResolver = new GitResolver('git://github.com/IndigoUnited/dejavu.git', {
+function testGitRemoteResolver() {
+    var dejavuResolver = new GitRemoteResolver('git://github.com/IndigoUnited/dejavu.git', {
         name: 'dejavu',
-        //target: '962be0f7b779b061eccce6a661928cb719031964'
-        //target: 'master'
-        target: '~0.4.1'
+        //target: '7d07190ca6fb7ffa63642526537e0c314cbaab12'
+        target: 'master'
+        //target: '~0.4.1'
     });
 
     return dejavuResolver.resolve()
@@ -16,6 +17,23 @@ function testGitResolver() {
     });
 }
 
+function testGitLocalResolver() {
+    var bowerResolver = new GitFsResolver('.', {
+        name: 'bower',
+        target: '*'
+    });
+
+    return bowerResolver.resolve()
+    .then(function () {
+        console.log('ok!');
+    }, function (err) {
+        console.log('failed to resolve', err);
+    });
+}
+
 if (process.argv[1] && !/mocha/.test(process.argv[1])) {
-    testGitResolver();
+    testGitRemoteResolver()
+    .then(testGitLocalResolver);
+
+    //testGitLocalResolver();
 }
