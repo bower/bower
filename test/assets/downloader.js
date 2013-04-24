030b6f972001117f52c2fb8016baa7a0638a4e11
André Cruz <andremiguelcruz@msn.com>
2013-04-24 01:00:51 +0100

    Remove util/fetchBranch.

--- test/assets/downloader.js
+++ test/assets/downloader.js
@@ -5,6 +5,18 @@ var fetchBranch = require('../util/fetchBranch');
 
 var githubTestPackage = path.join(__dirname, 'github-test-package');
 
+function fetchBranch(branch, dir) {
+    return cmd('git', ['checkout', '-b', branch, 'origin/' + branch], { cwd: dir })
+    .then(null, function (err) {
+        if (/already exists/i.test(err.details)) {
+            return cmd('git', ['checkout', branch], { cwd: dir })
+            .then(function () {
+                return cmd('git', ['pull', 'origin', branch], { cwd: dir });
+            });
+        }
+    });
+}
+
 function updateBranches() {
     console.log('Updating "test-package" branches..');
 
