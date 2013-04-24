d6e0638cd9c97b131b0d1cf680b881b1ec558202
André Cruz <andremiguelcruz@msn.com>
2013-04-24 01:11:30 +0100

    Oops.

--- test/assets/downloader.js
+++ test/assets/downloader.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 var path = require('path');
 var cmd = require('../../lib/util/cmd');
-var fetchBranch = require('../util/fetchBranch');
 
 var githubTestPackage = path.join(__dirname, 'github-test-package');
 
