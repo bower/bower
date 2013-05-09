423ce54d8a5f7e9933b1a0e53365752fc450c265
André Cruz <andremiguelcruz@msn.com>
2013-05-09 13:17:56 +0100

    Add empty tests and add cache TODO.

--- packages/bower-registry-client/lib/lookup.js
+++ packages/bower-registry-client/lib/lookup.js
@@ -16,14 +16,20 @@ function lookup(name, options, callback) {
     // Parse and set default options
     options = parseOptions.forRead(options);
 
-    // If no registry entries where passed, simply
+    // If no registry entries were passed, simply
     // error with package not found
     total = options.registry.length;
     if (!total) {
         return callback(createError('Package "' + name + '" not found', 'ENOTFOUND'));
     }
 
-    // Lookup package in series until we got its URL
+    // TODO: Add cache layer to avoid querying the registry always
+    //       The cache should be persistent and by-passable with a skipCache option
+    //       Beware that cache should take the configured registries in account
+    //       Ideally each registry should have an independent cache, so that each step
+    //       below would query the cache individually
+
+    // Lookup package in series until we got the URL
     async.doUntil(function (next) {
         var requestUrl = options.registry[current] + '/packages/' + encodeURIComponent(name);
 
