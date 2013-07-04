b8ba6e48276166be905a1ee7a1a3efadee1712d9
Sven Lito <me@svenlito.com>
2013-07-04 14:50:39 +0100

    adds index tests

---
+++ packages/bower-registry-client/test/core/index.js
@@ -0,0 +1,26 @@
+var index = require('../../lib/index'),
+    expect = require('chai').expect;
+
+describe('index module', function () {
+
+    describe('requiring the index module', function () {
+
+        it('should expose a lookup method', function () {
+            expect(index.lookup).to.be.ok;
+        });
+
+        it('should expose a list method', function () {
+            expect(index.list).to.be.ok;
+        });
+
+        it('should expose a register method', function () {
+            expect(index.register).to.be.ok;
+        });
+
+        it('should expose a search method', function () {
+            expect(index.search).to.be.ok;
+        });
+
+    });
+
+});
