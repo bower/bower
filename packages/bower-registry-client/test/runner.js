7e62e671e3fffab04d2fd19012c2b1e8b5ee8634
Sven Lito <me@svenlito.com>
2013-07-04 14:51:29 +0100

    updates testrunner

--- packages/bower-registry-client/test/runner.js
+++ packages/bower-registry-client/test/runner.js
@@ -1,2 +1,9 @@
 require('./Client');
+require('./core/search');
+require('./core/register');
+require('./core/lookup');
+require('./core/list');
+require('./core/index');
+require('./core/util/Cache');
+require('./core/util/createError');
 
