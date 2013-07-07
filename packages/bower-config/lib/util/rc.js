e152ab6cf2665065d8ed1a229e0b66eca926c0bf
André Cruz <andremiguelcruz@msn.com>
2013-07-07 02:26:06 +0100

    Use our own rc implementation.

--- packages/bower-config/lib/util/rc.js
+++ packages/bower-config/lib/util/rc.js
@@ -1,8 +1,10 @@
 var path = require('path');
+var fs = require('graceful-fs');
 var optimist = require('optimist');
 var osenv = require('osenv');
 var mout = require('mout');
-var cc = require('../../node_modules/rc/lib/utils');  // This is ugly!
+var rimraf = require('rimraf');
+var paths = require('./paths');
 
 var win = process.platform === 'win32';
 var home = osenv.home();
@@ -14,16 +16,75 @@ function rc(name, defaults, cwd, argv) {
 
     return mout.object.deepMixIn.apply(null, [
         defaults,
-        win ? {} : cc.json(path.join('/etc', name, 'config')),
-        win ? {} : cc.json(path.join('/etc', name + 'rc')),
-        cc.json(path.join(home, '.config', name, 'config')),
-        cc.json(path.join(home, '.config', name)),
-        cc.json(path.join(home, '.' + name, 'config')),
-        cc.json(path.join(home, '.' + name + 'rc')),
-        cc.json(path.join(cwd, '.' + name + 'rc')),
-        cc.env(name + '_'),
+        win ? {} : json(path.join('/etc', name + 'rc')),
+        json(path.join(home, '.' + name + 'rc')),
+        json(path.join(paths.config, name + 'rc')),
+        json(find('.' + name + 'rc', cwd)),
+        env(name + '_'),
         typeof argv.config !== 'object' ? {} : argv.config
     ]);
+}
+
+function parse(content, file) {
+    try {
+        return JSON.parse(content);
+    } catch (e) {
+        // If we got an error, remove the file
+        if (file) {
+            try {
+                rimraf.sync(file);
+            } catch (e) {}
+        }
+    }
+
+    return null;
+}
+
+function json(file) {
+    var content;
+
+    try {
+        content = fs.readFileSync(file);
+    } catch (err) {
+        return null;
+    }
+
+    return parse(content, file);
+}
+
+function env(prefix) {
+    var obj = {};
+    var prefixLength = prefix.length;
+
+    mout.object.forOwn(process.env, function (value, key) {
+        if (mout.string.startsWith(key, prefix)) {
+            obj[key.substr(prefixLength)] = value;
+        }
+    });
+
+    return obj;
+}
+
+function find(filename, dir) {
+    var walk = function (filename, dir) {
+        var file = path.join(dir, filename);
+        var parent = path.dirname(dir);
+
+        try {
+            fs.statSync(file);
+            return file;
+        } catch (err) {
+            // Check if we hit the root
+            if (parent === dir) {
+                return null;
+            }
+
+            return walk(filename, parent);
+        }
+    };
+
+    dir = dir || process.cwd();
+    return walk(filename, dir);
 }
 
 module.exports = rc;
