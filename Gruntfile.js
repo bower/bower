c7be3d73b97d7cf82f410499fbd8d9bf58a097df
Sven Lito <me@svenlito.com>
2013-07-11 19:15:49 +0100

    Close GH-611: Istanbul code coverage.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,14 +26,26 @@ module.exports = function (grunt) {
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint', 'simplemocha:short']
+        },
+        shell: {
+            cover: {
+                command: 'node node_modules/istanbul/lib/cli.js cover --dir ./test/reports node_modules/mocha/bin/_mocha -- -R dot',
+                options: {
+                    stdout: true,
+                    stderr: true
+                }
+            }
         }
+
     });
 
+    grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-execute');
 
     grunt.registerTask('test', ['execute:assets', 'simplemocha:full']);
+    grunt.registerTask('cover', 'shell:cover');
     grunt.registerTask('default', ['jshint', 'test']);
 };
