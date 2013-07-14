7d2162b2d2edf1ac55fd34c5983bd8772f06d3c2
André Cruz <andremiguelcruz@msn.com>
2013-07-14 21:45:43 +0100

    Fix order of events/interceptors.

--- test/core/logger.js
+++ test/core/logger.js
@@ -35,91 +35,91 @@ describe('Logger', function () {
             baz: 'string'
         };
 
-        it('should pass through {}', function (done) {
+        it('should pass through {}', function (next) {
             logger.on('log', function (log) {
                 expect(log.data).to.eql({});
-                done();
+                next();
             });
             logger.info();
         });
 
-        it('should pass through logData', function (done) {
+        it('should pass through logData', function (next) {
             logger.on('log', function (log) {
                 expect(log.data).to.eql(logData);
-                done();
+                next();
             });
             logger.info('foo', 'message', logData);
         });
 
-        it('should emit error event', function (done) {
+        it('should emit error event', function (next) {
             logger.on('log', function (log) {
                 expect(log.level).to.eql('error');
                 expect(log.id).to.eql('foo');
                 expect(log.message).to.eql('error message');
                 expect(log.data).to.eql({});
-                done();
+                next();
             });
             logger.error('foo', 'error message');
         });
 
-        it('should emit conflict event', function (done) {
+        it('should emit conflict event', function (next) {
             logger.on('log', function (log) {
                 expect(log.level).to.eql('conflict');
                 expect(log.id).to.eql('foo');
                 expect(log.message).to.eql('conflict message');
                 expect(log.data).to.eql({});
-                done();
+                next();
             });
             logger.conflict('foo', 'conflict message');
         });
 
-        it('should emit warn event', function (done) {
+        it('should emit warn event', function (next) {
             logger.on('log', function (log) {
                 expect(log.level).to.eql('warn');
                 expect(log.id).to.eql('foo');
                 expect(log.message).to.eql('warn message');
                 expect(log.data).to.eql({});
-                done();
+                next();
             });
             logger.warn('foo', 'warn message');
         });
 
-        it('should emit action event', function (done) {
+        it('should emit action event', function (next) {
             logger.on('log', function (log) {
                 expect(log.level).to.eql('action');
                 expect(log.id).to.eql('foo');
                 expect(log.message).to.eql('action message');
                 expect(log.data).to.eql({});
-                done();
+                next();
             });
             logger.action('foo', 'action message');
         });
 
-        it('should emit info event', function (done) {
+        it('should emit info event', function (next) {
             logger.on('log', function (log) {
                 expect(log.level).to.eql('info');
                 expect(log.id).to.eql('foo');
                 expect(log.message).to.eql('info message');
                 expect(log.data).to.eql({});
-                done();
+                next();
             });
             logger.info('foo', 'info message');
         });
 
-        it('should emit debug event', function (done) {
+        it('should emit debug event', function (next) {
             logger.on('log', function (log) {
                 expect(log.level).to.eql('debug');
                 expect(log.id).to.eql('foo');
                 expect(log.message).to.eql('debug message');
                 expect(log.data).to.eql({});
-                done();
+                next();
             });
             logger.debug('foo', 'debug message');
         });
     });
 
     describe('.intercept', function () {
-        it('should add the function and call it when a log occurs', function () {
+        it('should add the function and call it when a log occurs', function (next) {
             var called;
             var data = {
                 'some': 'thing'
@@ -134,13 +134,18 @@ describe('Logger', function () {
                     message: 'bar',
                     data: data
                 });
+                expect(log.data).to.equal(data);
             });
 
             logger.log('warn', 'foo', 'bar', data);
-            expect(called).to.be(true);
+
+            setTimeout(function () {
+                expect(called).to.be(true);
+                next();
+            }, 20);
         });
 
-        it('should call the interceptors by order before emitting the event', function () {
+        it('should call the interceptors by order before emitting the event', function (next) {
             var called = [];
 
             logger.intercept(function () {
@@ -151,7 +156,37 @@ describe('Logger', function () {
             });
 
             logger.log('warn', 'foo', 'bar');
-            expect(called).to.eql([1, 2]);
+
+            setTimeout(function () {
+                expect(called).to.eql([1, 2]);
+                next();
+            }, 20);
+        });
+
+        it('should call the interceptors along the chain', function (next) {
+            var called = [];
+            var childLogger = logger.geminate();
+
+            childLogger.intercept(function () {
+                called.push(1);
+            });
+            logger.intercept(function () {
+                called.push(2);
+            });
+
+            childLogger.on('log', function () {
+                called.push(3);
+            });
+            logger.on('log', function () {
+                called.push(4);
+            });
+
+            childLogger.log('warn', 'foo', 'bar');
+
+            setTimeout(function () {
+                expect(called).to.eql([1, 2, 3, 4]);
+                next();
+            }, 20);
         });
     });
 
@@ -161,7 +196,7 @@ describe('Logger', function () {
             expect(logger.pipe(otherEmitter)).to.equal(otherEmitter);
         });
 
-        it('should pipe log events to another emitter', function () {
+        it('should pipe log events to another emitter', function (next) {
             var otherEmitter = new EventEmitter();
             var data = {
                 'some': 'thing'
@@ -181,7 +216,11 @@ describe('Logger', function () {
             });
 
             logger.log('warn', 'foo', 'bar', data);
-            expect(piped).to.be(true);
+
+            setTimeout(function () {
+                expect(piped).to.be(true);
+                next();
+            }, 20);
         });
     });
 
@@ -194,25 +233,40 @@ describe('Logger', function () {
             expect(newLogger).to.not.be.equal(logger);
         });
 
-        it('should pipe the new logger events to the original logger', function () {
-            var piped;
-            var newLogger = logger.geminate();
+        it('should pipe the new logger events to the original logger', function (next) {
+            var piped = [];
+            var childLogger = logger.geminate();
             var data = {
                 'some': 'thing'
             };
 
-            logger.on('log', function (log) {
-                piped = true;
+            childLogger.on('log', function (log) {
+                piped.push(1);
                 expect(log).to.eql({
                     level: 'warn',
                     id: 'foo',
                     message: 'bar',
                     data: data
                 });
+                expect(log.data).to.equal(data);
             });
 
-            newLogger.log('warn', 'foo', 'bar', data);
-            expect(piped).to.be(true);
+            logger.on('log', function (log) {
+                piped.push(2);
+                expect(log).to.eql({
+                    level: 'warn',
+                    id: 'foo',
+                    message: 'bar',
+                    data: data
+                });
+                expect(log.data).to.equal(data);
+            });
+
+            childLogger.log('warn', 'foo', 'bar', data);
+            setTimeout(function () {
+                expect(piped).to.eql([1, 2]);
+                next();
+            }, 20);
         });
     });
 });
