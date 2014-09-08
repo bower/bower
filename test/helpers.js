1bb0d8f823dcdc2670c240b18fae46a71ebe3819
Adam Stankiewicz <sheerun@sher.pl>
2014-09-08 03:02:57 +0200

    [test] Add tests for git repository install

--- test/helpers.js
+++ test/helpers.js
@@ -5,8 +5,12 @@ var rimraf = require('rimraf');
 var uuid = require('node-uuid');
 var object = require('mout/object');
 var fs = require('fs');
-var object = require('mout/object');
+var glob = require('glob');
+var os = require('os');
+var cmd = require('../lib/util/cmd');
 var config = require('../lib/config');
+
+var tmpLocation = path.join(os.tmpdir ? os.tmpdir() : os.tmpDir(), 'bower-tests');
 
 exports.require = function (name) {
     return require(path.join(__dirname, '../', name));
@@ -18,23 +22,19 @@ beforeEach(function () {
 });
 
 after(function () {
-    rimraf.sync(path.join(__dirname, 'tmp'));
+    rimraf.sync(path.join(tmpLocation, 'tmp'));
 });
 
 exports.TempDir = (function() {
-    function TempDir (defaultFiles) {
-        this.path = path.join(__dirname, 'tmp/' + uuid.v4());
-        this.defaultFiles = defaultFiles;
+    function TempDir (defaults) {
+        this.path = path.join(tmpLocation, 'tmp/' + uuid.v4());
+        this.defaults = defaults;
     }
 
-    TempDir.prototype.prepare = function (files) {
+    TempDir.prototype.create = function (files) {
         var that = this;
 
-        files = object.merge(files || {}, this.defaultFiles);
-
-        rimraf.sync(that.path);
-
-        mkdirp.sync(that.path);
+        files = object.merge(files || {}, this.defaults);
 
         if (files) {
             object.forOwn(files, function (contents, filepath) {
@@ -49,6 +49,57 @@ exports.TempDir = (function() {
         }
 
         return this;
+    };
+
+    TempDir.prototype.prepare = function (files) {
+        rimraf.sync(this.path);
+        mkdirp.sync(this.path);
+        this.create(files);
+
+        return this;
+    };
+
+    // TODO: Rewrite to synchronous form
+    TempDir.prototype.prepareGit = function (revisions) {
+        var that = this;
+
+
+        revisions = object.merge(revisions || {}, this.defaults);
+
+        rimraf.sync(that.path);
+
+        mkdirp.sync(that.path);
+
+        var promise = new Q();
+
+        object.forOwn(revisions, function (files, tag) {
+            promise = promise.then(function () {
+                return cmd('git', ['init'], { cwd: that.path });
+            }).then(function () {
+                that.glob('./!(.git)').map(function (removePath) {
+                    var fullPath = path.join(that.path, removePath);
+
+                    rimraf.sync(fullPath);
+                });
+
+                that.create(files);
+            }).then(function () {
+                return cmd('git', ['add', '-A'], { cwd: that.path });
+            }).then(function () {
+                return cmd('git', ['commit', '-m"commit"'], { cwd: that.path });
+            }).then(function () {
+                return cmd('git', ['tag', tag], { cwd: that.path });
+            });
+        });
+
+        return promise;
+    };
+
+    TempDir.prototype.glob = function (pattern) {
+        return glob.sync(pattern, {
+            cwd: this.path,
+            dot: true
+        });
     };
 
     TempDir.prototype.read = function (name) {
