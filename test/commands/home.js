9d06fce5f919080ac817949dd7620bd8234feea3
Adam Stankiewicz <sheerun@sher.pl>
2015-01-05 05:31:12 +0100

    Add few more tests for home command

--- test/commands/home.js
+++ test/commands/home.js
@@ -10,6 +10,12 @@ describe('bower home', function () {
         }
     });
 
+    var wrongPackage = new helpers.TempDir({
+        'bower.json': {
+            name: 'package'
+        }
+    });
+
     it('opens repository home page in web browser', function (done) {
         package.prepare();
 
@@ -22,4 +28,31 @@ describe('bower home', function () {
         return helpers.run(home, [package.path]);
     });
 
+    it('opens home page of current repository', function (done) {
+        package.prepare();
+
+        var home = helpers.command('home', {
+            opn: helpers.ensureDone(done, function(url) {
+                expect(url).to.be('http://bower.io');
+            })
+        });
+
+        return helpers.run(home, [undefined, { cwd: package.path }]);
+    });
+
+    it('errors if no homepage is set', function (done) {
+        wrongPackage.prepare();
+
+        var home = helpers.command('home', {
+            opn: helpers.ensureDone(done, function(url) {
+                expect(url).to.be('http://bower.io');
+            })
+        });
+
+        return helpers.run(home, [wrongPackage.path])
+        .fail(helpers.ensureDone(done, function(reason) {
+            expect(reason.message).to.be('No homepage set for package');
+            expect(reason.code).to.be('ENOHOME');
+        }));
+    });
 });
