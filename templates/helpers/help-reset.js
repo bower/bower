637dd5aa8978b0c883b0484e1006e884389751b8
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-05-13 21:18:44 +0100

    Resolves diverged code

---
+++ templates/helpers/help-reset.js
@@ -0,0 +1,14 @@
+{
+    "command": "reset",
+    "description": "The reset functionality allows developers to easily reinstall the packages.\n\nUsing 'bower reset' in a project folder will remove all installed packages and install them again.",
+    "usage": [
+        "reset"
+    ],
+    "options": [
+        {
+            "shorthand":   "-h",
+            "flag":        "--help",
+            "description": "Show this help message"
+        }
+    ]
+}
