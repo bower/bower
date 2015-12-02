026ef8ebb3db32095170337ab4ff795d9473592c
Sam Clift <sam@cloudfindhq.com>
2015-12-02 16:36:59 +0000

    Split out the testing of pre and post uninstall hooks in two tests

--- test/core/scripts.js
+++ test/core/scripts.js
@@ -54,13 +54,25 @@ describe('scripts', function () {
 
     });
 
-    it('should run preuninstall hook and postuninstall hooks', function (next) {
+    it('should run preuninstall hook.', function (next) {
 
         bower.commands
         .uninstall([packageName], undefined, config)
         .on('end', function (installed) {
 
             expect(fs.existsSync(path.join(tempDir, 'preuninstall_' + packageName))).to.be(true);
+
+            next();
+        });
+
+    });
+
+    it('should run postuninstall hook.', function (next) {
+
+        bower.commands
+        .uninstall([packageName], undefined, config)
+        .on('end', function (installed) {
+
             expect(fs.existsSync(path.join(tempDir, 'postuninstall_' + packageName))).to.be(true);
 
             next();
