afc4bfbd42d17113bf816666a2d08caff82e5f80
Alireza Bashiri <azbshiri@gmail.com>
2016-01-04 00:58:03 +0330

    Revert "Display login message when user login via normal and 2 factor auth mode."
    This reverts commit ed881e3f2989e56cac5050ebb11ffec6cbf6725b.

--- test/util/git.js
+++
@@ -1,8 +0,0 @@
-var expect = require('expect.js');
-var hasGit = require('../../lib/util/git');
-describe('Git installation check', function () {
-    it('Git should be installed', function (done) {
-        expect(hasGit()).to.be(true);
-        done();
-    });
-});
--- test/util/index.js
+++ test/util/index.js
@@ -2,5 +2,4 @@ describe('util', function () {
     require('./removeIgnores');
     require('./analytics');
     require('./download');
-    require('./git');
 });
