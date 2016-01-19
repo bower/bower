3e3b64218d0a266a900cdcffcc7ef6b8f568aee9
Adam Stankiewicz <sheerun@sher.pl>
2016-01-19 20:27:35 +0100

    Remove analytics from Bower, fixes #1102
    They caused more issues than useful they were.
    
    Instead, we'll focus on fetching statistics from
    NPM registry to watch Bower's popularity, and
    GitHub stars over time to watch packages' popularity.

--- test/util/index.js
+++ test/util/index.js
@@ -1,6 +1,5 @@
 describe('util', function () {
     require('./removeIgnores');
-    require('./analytics');
     require('./download');
     require('./isPathAbsolute');
     require('./relativeToBaseDir');
